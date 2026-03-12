487e7be197c9462b77de33bbec88e2fd90389f7b
Kodex <isaacaryeepah@gmail.com>
2026-03-12 12:08:50 +0000

    Add files via upload

---
+++ src/routes/questionBank_route.js
@@ -0,0 +1,21 @@
+const express  = require("express");
+const authenticate = require("../middleware/auth");
+const { requireRole, requireMode } = require("../middleware/role");
+const { requireActiveSubscription } = require("../middleware/subscription");
+const ctrl = require("../controllers/questionBankController");
+
+const router = express.Router();
+
+router.use(authenticate);
+router.use(requireMode("academic"));
+router.use(requireActiveSubscription);
+router.use(requireRole("lecturer", "superadmin"));
+
+router.get("/",                    ctrl.list);
+router.post("/",                   ctrl.create);
+router.put("/:id",                 ctrl.update);
+router.delete("/:id",              ctrl.remove);
+router.post("/save-from-quiz",     ctrl.saveFromQuiz);
+router.post("/import-to-quiz",     ctrl.importToQuiz);
+
+module.exports = router;
